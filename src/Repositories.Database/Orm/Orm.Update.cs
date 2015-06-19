f68ce08916e7350957eebcc394e9ee552cacd284
Jacob <jacob.m.celletti@gmail.com>
2015-06-18 21:02:41 -0700

    add all orm bits

---
+++ src/Repositories.Database/Orm/Orm.Update.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using System.Reflection;
+using System.Text;
+
+namespace JMC.Repositories.Database.Orm
+{
+	public partial class Orm<TEntity>
+	{
+		public static void Update(SqlCommand command, TEntity item)
+		{
+			TableInformation table = Orm<TEntity>.GetTableInformation();
+			var sb = new StringBuilder(string.Format("UPDATE {0} SET ", table.TableName));
+
+			foreach (PropertyInfo pi in Orm<TEntity>.GetProperties())
+			{
+				sb.AppendFormat("{0} = ", pi.Name);
+
+				if (Orm<TEntity>.UseQuotedValue(pi.PropertyType))
+				{
+					sb.AppendFormat("'{0}', ", pi.GetValue(item));
+				}
+				else
+				{
+					sb.AppendFormat("{0}, ", pi.GetValue(item));
+				}
+			}
+
+			sb.Remove(sb.Length - 2, 1);
+
+			sb.AppendFormat("WHERE Id = '{0}'", item.Id);
+
+			command.CommandText = sb.ToString();
+
+			command.ExecuteNonQuery();
+		}
+	}
+}
